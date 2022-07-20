bcb28c7cd7870b698c983943e2aeabc8a01be23d
kwietkannon <72056579+kwietkannon@users.noreply.github.com>
2022-07-20 14:41:31 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
-dnf install -y git lxappearance
-dnf copr enable frosty/qtile
-dnf install qtile
-dnf install qtile-extra
-cd ~/Downloads
-got clone https://github.com/stronk-dev/Tokyo-Night-Linux.git
-cp -a ~/Downloads/Tokyo-Night-Linux/usr/share/themes/TokyoNight /usr/share/themes
+sudo dnf -y update
+yes | dnf copr enable frostyx/qtile
+dnf install -y git lxappearance qtile qtile-extras
+cd /home/$USER/Downloads
+git clone https://github.com/stronk-dev/Tokyo-Night-Linux.git
+cp -a /home/$USER/Downloads/Tokyo-Night-Linux/usr/share/themes/TokyoNight /usr/share/themes
 wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
 unzip Hack-v3.003-ttf.zip
-cp -a ~/Downloads/ttf/* /usr/share/fonts
+cp -a /home/$USER/Downloads/ttf/* /usr/share/fonts
 fc-cache -f -v
  
