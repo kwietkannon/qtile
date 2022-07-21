93904f30616dadbd217a695c9ac019fdae6dd195
kwietkannon <72056579+kwietkannon@users.noreply.github.com>
2022-07-21 10:38:47 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -22,9 +22,9 @@ sudo systemctl set-default graphical.target
 ##### RPM Fusion Repos
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf group update core
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf groupupdate sound-and-video
+sudo dnf group update core -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y 
+sudo dnf groupupdate sound-and-video -y
 
 
 
