612455f46ca6d7e3e832556ec684e252916613b4
kwietkannon <72056579+kwietkannon@users.noreply.github.com>
2022-07-21 10:22:40 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 sudo dnf -y update
+sudo usermod -aG wheel $USER
 yes | dnf copr enable frostyx/qtile 
-dnf install -y git lxappearance pcmanfm qtile qtile-extras kitty picom nitrogen NetworkManager-wifi
+# not sure if I need @base-x was using it for lightdm. 
+dnf install -y git lxappearance pcmanfm qtile qtile-extras kitty picom nitrogen NetworkManager-wifi wget unzip lxdm @base-x dmenu dunst autocpu-freq vim
 cd /home/$USER/Downloads
 git clone https://github.com/stronk-dev/Tokyo-Night-Linux.git
 cp -a /home/$USER/Downloads/Tokyo-Night-Linux/usr/share/themes/TokyoNight /usr/share/themes
@@ -11,8 +13,10 @@ unzip Hack-v3.003-ttf.zip
 cp -a /home/$USER/Downloads/ttf/* /usr/share/fonts
 fc-cache -f -v
 git clone https://github.com/kwietkannon/qtile
-sudo rm /home/$USER/.config/qtile/config.py
-sudo cp -a qtile/config.py /home/$USER/.config/qtile/
+sudo mkdir -p /home/$USER/.config/qtile && cp -a qtile/config.py /home/$USER/.config/qtile/
 sudo cp -a qtile/autostart.sh /home/$USER/.config/qtile/
 sudo chmod +x /home/$USER/.config/qtile/autostart.sh
+sudo systemctl enable lightdm.service
+sudo systemctl set-default graphical.target
+
  
