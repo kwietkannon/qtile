365eb172ca6116e41e3b49514976c6811f2b2e1d
kwietkannon <72056579+kwietkannon@users.noreply.github.com>
2022-07-21 11:05:48 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ sudo usermod -aG wheel $USER
 yes | dnf copr enable frostyx/qtile 
 # not sure if I need @base-x was using it for lightdm. 
 dnf install -y git lxappearance pcmanfm qtile qtile-extras kitty picom nitrogen NetworkManager-wifi wget unzip lxdm @base-x rofi dunst autocpu-freq vim firefox \
-xautolock i3lock-fancy
+xautolock i3lock
 cd /home/$USER/Downloads
 git clone https://github.com/stronk-dev/Tokyo-Night-Linux.git
 cp -a /home/$USER/Downloads/Tokyo-Night-Linux/usr/share/themes/TokyoNight /usr/share/themes
