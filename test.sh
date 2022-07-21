549c2a85483fcb0855bb2d974bdfaec0cb30d9e8
kwietkannon <72056579+kwietkannon@users.noreply.github.com>
2022-07-21 08:25:26 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 sudo dnf -y update
-yes | dnf copr enable frostyx/qtile
-dnf install -y git lxappearance qtile qtile-extras kitty
+yes | dnf copr enable frostyx/qtile 
+dnf install -y git lxappearance pcmanfm qtile qtile-extras kitty picom nitrogen NetworkManager-wifi
 cd /home/$USER/Downloads
 git clone https://github.com/stronk-dev/Tokyo-Night-Linux.git
 cp -a /home/$USER/Downloads/Tokyo-Night-Linux/usr/share/themes/TokyoNight /usr/share/themes
@@ -10,4 +10,9 @@ wget https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003
 unzip Hack-v3.003-ttf.zip
 cp -a /home/$USER/Downloads/ttf/* /usr/share/fonts
 fc-cache -f -v
+git clone https://github.com/kwietkannon/qtile
+sudo rm /home/$USER/.config/qtile/config.py
+sudo cp -a qtile/config.py /home/$USER/.config/qtile/
+sudo cp -a qtile/autostart.sh /home/$USER/.config/qtile/
+sudo chmod +x /home/$USER/.config/qtile/autostart.sh
  
