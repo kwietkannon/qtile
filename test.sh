09ff983e04e9bc9e92362ac5f217ead4ce9983db
kwietkannon <72056579+kwietkannon@users.noreply.github.com>
2022-07-22 11:57:44 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -27,7 +27,7 @@ sudo systemctl enable lxdm.service
 sudo systemctl set-default graphical.target
 
 ##### RPM Fusion Repos
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+echo yes| sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf group update core -y
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y 
